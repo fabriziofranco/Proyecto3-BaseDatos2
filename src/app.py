724901d885b0cb6c8c09e93cfe8c001fc1353596
Christian Ledgard <39962750+christianledgard@users.noreply.github.com>
2020-12-06 16:12:12 -0500

    Front end Input and Output

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, Response, flash
+from flask import Flask, render_template, request, redirect, url_for, Response, flash, send_from_directory
 import json
 import importlib
 import sys
 import os 
 import glob
-from index import Query
-import index
 import time
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -21,36 +19,40 @@ app.secret_key = b'heiderEsLoMaximo/'
 def home():
    return render_template('buscador.html')
 
-@app.route('/consulta', methods = ['POST'])
-def consulta():
-   message = json.loads(request.data)
-   words =  message['values']
-   cantidad = message['cantidad']
-   data = Query().query(words,cantidad)
-   response = Response(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-   return response
 
-@app.route('/upload', methods = ['POST'])
-def upload():
+@app.route('/search', methods = ['POST'])
+def search():
    uploaded_files = request.files.getlist("file")
-   numero_bloques = request.form.get("numeroBloques")
+   numero_bloques = request.form.get("numElement")
    
-   files = glob.glob('clean/*') + glob.glob('clean_likeADict/*') + glob.glob('inverted_index/*') + glob.glob('merging_blocks/*') + glob.glob('sorted_blocks/*')
+   files = glob.glob('data/imageInput/*')
    for f in files:
       os.remove(f)
 
    for file in uploaded_files:
-      with open("clean/"+str(file.filename), "wb") as archivo:
+      with open("data/imageInput/"+str(file.filename), "wb") as archivo:
          archivo.write(file.read())
    
-   index.Index("clean", "inverted_index", "merging_blocks", "sorted_blocks", int(numero_bloques))
+   #index.Index("clean", "inverted_index", "merging_blocks", "sorted_blocks", int(numero_bloques))
    
    flash(u'Los datos se han cargado de manera correcta.',  'alert-success')
-   return render_template('buscador.html')
 
+   images_output = list()
+   for f in glob.glob('data/imageOutput/*'):
+      tempImage = dict()
+      tempImage["url"] = f
+      tempImage["accuracy"] = 0.8
+      images_output.append(tempImage)
+
+
+   print(images_output)
+   return render_template('buscador.html', images_output=images_output)
+
+
+
+@app.route('/data/imageOutput/<path:filename>')
+def base_static(filename):
+    return send_from_directory('data/imageOutput/', filename)
+   
 if __name__ == '__main__':
    app.run()
