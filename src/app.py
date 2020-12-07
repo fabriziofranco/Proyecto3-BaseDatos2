c4d10ebde7aaf926e6d640569bd95a8eb265de01
Christian Ledgard <39962750+christianledgard@users.noreply.github.com>
2020-12-07 12:38:55 -0500

    Front End Range

--- src/app.py
+++ src/app.py
@@ -24,7 +24,16 @@ def home():
 
 @app.route('/search', methods = ['POST'])
 def search():
-   num_results = int(request.form.get("numElement"))
+   type_of_search = request.form.get("type")
+
+   if type_of_search == "knn":
+      num_results = int(request.form.get("numElement"))
+   elif type_of_search == "rango":
+      range_results = float(request.form.get("numRange"))
+   else:
+      flash('Seleccionar el tipo de búsqueda.', 'alert-danger')
+      return redirect(url_for('home'))
+
 
    if 'file' not in request.files:
       flash('No hay archivo, intente nuevamente.', 'alert-danger')
@@ -46,10 +55,18 @@ def search():
    start = time.time()
    archivo =  open("data/imageInput/"+str(file.filename), "wb")
    archivo.write(file.read())
-   list_of_path = rtree_index.KNN_FaceRecognition(str(file.filename), num_results, "12800", True)
-   end = time.time()
 
-   flash(u'Se han encontrado ' + str(num_results) + ' resultados en ' + str(end - start) + ' segundos.',  'alert-success')
+   if type_of_search == "rango":
+      list_of_path = rtree_index.range_search_rtree(str(file.filename), range_results, "12800", True)
+      end = time.time()
+      flash(u'Se han encontrado los resultados en ' + str(end - start) + ' segundos utilizando la búsqueda por rango.',  'alert-success')
+   
+   else:
+      list_of_path = rtree_index.KNN_FaceRecognition(str(file.filename), num_results, "12800", True)
+      end = time.time()
+      flash(u'Se han encontrado ' + str(num_results) + ' resultados en ' + str(end - start) + ' segundos utilizando la búsqueda KNN.',  'alert-success')
+   
+
 
    images_output = list()
    for f in list_of_path:
